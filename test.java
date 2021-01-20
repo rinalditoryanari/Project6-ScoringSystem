33c7ed9405fbc94fb58029a91d51dbd9aa75dcd9
Rinaldito <rinalditoryanari@gmail.com>
2021-01-20 19:44:13 +0700

    Peojek 6

---
+++ test.java
@@ -0,0 +1,121 @@
+public class test{
+    public static void main(String[] args) {
+            
+        int id, id1, nilai, nilai1; String nama, nama1;
+        System.out.println("Selamat Datang di Scoring System");
+        System.out.println("");
+        System.out.println("A. Guru");
+        System.out.println("B. Murid");
+        System.out.println("C. Keluar");
+        System.out.println("Silahkan Pilih Untuk Login: A");
+        
+        System.out.println("----------- Guru -----------");
+        System.out.println("A. Input Nilai");
+        System.out.println("B. Lihat Nilai");
+        System.out.println("C. Keluar");
+        System.out.println("Silahkan pilih operasi: A");
+        
+        System.out.println("~~~ INPUT NILAI ~~~");
+        System.out.println("Silahkan Masukkan Id Siswa: 001");
+        id= 001;
+        System.out.println("Masukkan Nama Siswa: Dito");
+        nama= "Dito";
+        System.out.println("Masukkan Nilai Akhir: 50");
+        nilai= 50;
+        
+        System.out.println("");
+        System.out.println("-------------------");
+        System.out.println("DATA TELAH DIINPUT");
+        System.out.println("ID: " + id);
+        System.out.println("Nama: " + nama);
+        System.out.println("Nilai: " + nilai);
+        System.out.println("-------------------");
+        System.out.println("");
+        
+        System.out.println("A. Input Nilai");
+        System.out.println("B. Lihat Nilai");
+        System.out.println("C. Keluar");
+        System.out.println("Silahkan pilih operasi: A");
+        
+        System.out.println("~~~ INPUT NILAI ~~~");
+        System.out.println("Silahkan Masukkan Id Siswa: 002");
+        id1 = 002;
+        System.out.println("Masukkan Nama Siswa: Mail");
+        nama1 = "Mail";
+        System.out.println("Masukkan Nilai Akhir: 50");
+        nilai1 = 50;
+        
+        System.out.println("");
+        System.out.println("-------------------");
+        System.out.println("DATA TELAH DIINPUT");
+        System.out.println("ID: " + id1);
+        System.out.println("Nama: " + nama1);
+        System.out.println("Nilai: " + nilai1);
+        System.out.println("-------------------");
+        System.out.println("");
+        
+        System.out.println("A. Input Nilai");
+        System.out.println("B. Lihat Nilai");
+        System.out.println("C. Keluar");
+        System.out.println("Silahkan pilih operasi: B");
+        
+        System.out.println("~~~ LIHAT NILAI ~~~");
+        
+        System.out.println("-------------------");
+        System.out.println("ID: " + id);
+        System.out.println("Nama: " + nama);
+        System.out.println("Nilai: " + nilai);
+        System.out.println("-------------------");
+        System.out.println("-------------------");
+        System.out.println("ID: " + id1);
+        System.out.println("Nama: " + nama1);
+        System.out.println("Nilai: " + nilai1);
+        System.out.println("-------------------");
+        
+        System.out.println("A. Input Nilai");
+        System.out.println("B. Lihat Nilai");
+        System.out.println("C. Keluar");
+        System.out.println("Silahkan pilih operasi: C");
+        
+        System.out.println("");
+        System.out.println("A. Guru");
+        System.out.println("B. Murid");
+        System.out.println("C. Keluar");
+        System.out.println("Silahkan Pilih Untuk Login: B");
+        
+        System.out.println("----------- Murid -----------");
+        System.out.println("Silahkan Masukkan Id Siswa: 001");
+
+        System.out.println("~~~ NILAI SISWA ~~~");
+        System.out.println("ID: " + id);
+        System.out.println("Nama: " + nama);
+        System.out.println("Nilai: " + nilai);
+        System.out.println("-------------------");
+        
+        System.out.println("");
+        System.out.println("A. Guru");
+        System.out.println("B. Murid");
+        System.out.println("C. Keluar");
+        System.out.println("Silahkan Pilih Untuk Login: B");
+        
+        
+        System.out.println("----------- Murid -----------");
+        System.out.println("Silahkan Masukkan Id Siswa: 002");
+
+        System.out.println("~~~ NILAI SISWA ~~~");
+        System.out.println("ID: " + id1);
+        System.out.println("Nama: " + nama1);
+        System.out.println("Nilai: " + nilai1);
+        System.out.println("-------------------");
+        
+        
+        System.out.println("");
+        System.out.println("A. Guru");
+        System.out.println("B. Murid");
+        System.out.println("C. Keluar");
+        System.out.println("Silahkan Pilih Untuk Login: C");
+        
+        System.out.println("-------- TERIMAKASIH --------");
+        
+    }    
+}
